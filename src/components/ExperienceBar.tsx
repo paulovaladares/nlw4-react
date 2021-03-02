0c33a9144c120267b1959d496233772ce9d27448
Paulo Valadares <contato@paulovaladares.com.br>
2021-03-02 01:15:28 -0300

    Fix js lint

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -4,7 +4,7 @@ export function ExperienceBar() {
            <span>0 xp</span>
            <div>
                <div style={{ width: '68%'}}></div>
-               <span className="current-experience" style={{ left: '68%'}}>68</span>
+               <span className="current-experience" style={{ left: '68%' }}>68</span>
            </div>
            <span>600 xp</span>
        </header>
